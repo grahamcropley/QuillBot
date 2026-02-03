1483899d0dfc3592972b2c93c34678335d413643
Graham Cropley <graham.cropley@gmail.com>
2026-02-03 17:54:47 +0000

    fix: use js next config in production

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const nextConfig = {
+  turbopack: {},
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
